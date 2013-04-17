fec355dfa27421c0a08d9345717a7f9fb6dd89b3
James Aylett <james@tartarus.org>
2013-04-17 14:44:26 +0100

    Run gunicorn + dummy process; python will try to push to statsd

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b "0.0.0.0:$PORT" -w 4 web:app
+web: USER=nobody PORT=$PORT honcho -f Procfile.chain start
--- web.py
+++ web.py
@@ -1,8 +1,11 @@
+import statsd
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    counter = statsd.Counter("Homepage hits")
+    counter += 1
     return "Hello World!"
 
 if __name__ == "__main__":
